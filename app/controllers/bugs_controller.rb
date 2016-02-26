ee6f57f43ef6bc3355e64c5749f04abc71373465
Krishna <karumank@mail.gvsu.edu>
2016-02-26 01:45:00 +0000

    User display fix

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -33,6 +33,8 @@ class BugsController < ApplicationController
       if @bug.save
         format.html { redirect_to @bug, notice: 'Bug was successfully created.' }
         format.json { render :show, status: :created, location: @bug }
+        user = User.find(bug_params[:user_id])
+        @bug.build_user(:id => user.id)
       else
         format.html { render :new }
         format.json { render json: @bug.errors, status: :unprocessable_entity }
@@ -80,6 +82,6 @@ class BugsController < ApplicationController
     end
     # Never trust parameters from the scary internet, only allow the white list through.
     def bug_params
-      params.require(:bug).permit(:title, :despriction, :issue_type, :priority, :status)
+      params.require(:bug).permit(:title, :despriction, :issue_type, :priority, :status, :user_id)
     end
 end
