e9a13b6dc9e5f04cd331d8eaf765cb2c52010a04
Krishna <karumank@mail.gvsu.edu>
2016-03-16 23:17:55 +0000

    Initial CSS Changes

---
+++ app/assets/javascripts/bugs.js
@@ -0,0 +1,17 @@
+//  $(document).ready(function(){
+//      alert("hi from bugs js");
+// //  var myTabs = tabs({
+// //     el: '#tabs',
+// //     tabNavigationLinks: '.c-tabs-nav__link',
+// //     tabContentContainers: '.c-tab'
+// //   });
+
+// //   myTabs.init();
+  
+// //   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// //   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// //   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// //   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+// //   ga('create', 'UA-34160351-1', 'auto');
+// //   ga('send', 'pageview');
+//  });
